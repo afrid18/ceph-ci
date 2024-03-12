cdcf4698ee22c229444f9484ca7f42620cc210e7
Casey Bodley <cbodley@redhat.com>
2024-03-12 16:25:58 -0400

    rgw/pubsub: when present, use account id instead of tenant
    RGWPubSub provides topic namespace isolation for tenants by adding
    prefixes to rados object names and topic metadata keys. accounts use
    this the same way
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -11028,7 +11028,8 @@ next:
         return -ret;
       }
     } else {
-      RGWPubSub ps(driver, tenant, *site);
+      const std::string& account = !account_id.empty() ? account_id : tenant;
+      RGWPubSub ps(driver, account, *site);
       const RGWPubSub::Bucket b(ps, bucket.get());
       ret = b.get_topics(dpp(), result, null_yield);
       if (ret < 0 && ret != -ENOENT) {
@@ -11041,7 +11042,8 @@ next:
   }
 
   if (opt_cmd == OPT::PUBSUB_TOPIC_LIST) {
-    RGWPubSub ps(driver, tenant, *site);
+    const std::string& account = !account_id.empty() ? account_id : tenant;
+    RGWPubSub ps(driver, account, *site);
     std::string next_token = marker;
 
     std::optional<rgw_owner> owner;
@@ -11099,7 +11101,8 @@ next:
       cerr << "ERROR: topic name was not provided (via --topic)" << std::endl;
       return EINVAL;
     }
-    RGWPubSub ps(driver, tenant, *site);
+    const std::string& account = !account_id.empty() ? account_id : tenant;
+    RGWPubSub ps(driver, account, *site);
 
     rgw_pubsub_topic topic;
     std::set<std::string> subscribed_buckets;
@@ -11143,7 +11146,8 @@ next:
         return -ret;
       }
     } else {
-      RGWPubSub ps(driver, tenant, *site);
+      const std::string& account = !account_id.empty() ? account_id : tenant;
+      RGWPubSub ps(driver, account, *site);
       const RGWPubSub::Bucket b(ps, bucket.get());
       ret = b.get_topics(dpp(), bucket_topics, null_yield);
       if (ret < 0 && ret != -ENOENT) {
@@ -11170,7 +11174,8 @@ next:
       return -EINVAL;
     }
 
-    RGWPubSub ps(driver, tenant, *site);
+    const std::string& account = !account_id.empty() ? account_id : tenant;
+    RGWPubSub ps(driver, account, *site);
 
     ret = ps.remove_topic(dpp(), topic_name, null_yield);
     if (ret < 0) {
@@ -11210,7 +11215,8 @@ next:
       ret = remove_notification_v2(dpp(), driver, bucket.get(), notification_id,
                                    null_yield);
     } else {
-      RGWPubSub ps(driver, tenant, *site);
+      const std::string& account = !account_id.empty() ? account_id : tenant;
+      RGWPubSub ps(driver, account, *site);
 
       rgw_pubsub_bucket_topics bucket_topics;
       const RGWPubSub::Bucket b(ps, bucket.get());
